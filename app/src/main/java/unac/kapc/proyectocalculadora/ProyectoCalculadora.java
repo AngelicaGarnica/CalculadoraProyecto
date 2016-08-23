a9be1651de7fca27887a2a63bd2a21b7d0144db5
Laboratorio de Software <lds@unac.edu.co>
2016-08-22 19:25:06 -0500

    Initial commit

---
+++ app/src/main/java/unac/kapc/proyectocalculadora/ProyectoCalculadora.java
@@ -0,0 +1,13 @@
+package unac.kapc.proyectocalculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProyectoCalculadora extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proyecto_calculadora);
+    }
+}
